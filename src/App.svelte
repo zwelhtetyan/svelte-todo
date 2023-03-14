89d050be52bd6d09b9288f4cb2c5c3ff91a22f00
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-15 00:09:34 +0630

    test svelte by making todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
-  const name = 'zwel'
+  import { null_to_empty } from 'svelte/internal';
+  import { writable } from 'svelte/store';
+  import { fade } from 'svelte/transition';
+  import type { TodoItem } from './types';
 
+  const todoStore = writable<TodoItem[]>([]);
+  let inputRef: HTMLInputElement;
+
+  function addTodo() {
+    if (!inputRef.value.trim()) return;
+
+    const newTodo: TodoItem = {
+      id: new Date().getTime().toString(),
+      taskName: inputRef.value,
+      done: false,
+    };
+
+    todoStore.update((prevTodos) => [...prevTodos, newTodo]);
+
+    inputRef.value = '';
+  }
 </script>
 
 <main>
-  {#if true}<h1>name is {name}</h1>{:else }<h1>nooo....</h1>{/if}
+  <form on:submit|preventDefault="{addTodo}">
+    <input type="text" bind:this="{inputRef}" />
+    <button>+</button>
+  </form>
 
-
+  {#if $todoStore.length > 0}
+    <ol>
+      {#each $todoStore as todo}
+        <li transition:fade>
+          {todo.taskName}
+        </li>
+      {/each}
+    </ol>
+  {:else}
+    <p>Please make your first todo.</p>
+  {/if}
 </main>
-
